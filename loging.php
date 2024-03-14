5b139a5e07f47caa3415cd933aba7a97b1b92d35
Serch <sergiobonilla182@gmail.com>
2024-03-14 10:03:14 -0600

    Creado el archivo login.php

---
+++ loging.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="form-group">
+    <input type="email" placeholder="Ingrese su correo" name="email" id="email">
+    <br>
+    <input type="password" placeholder="Ingrese su contraseña" name="password" id="password">
+    <br>
+    <input type="submit" value="Ingresar">
+</div>
+
+
+    <!-- Incluir Bootstrap JS (opcional) -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <!-- Opcionalmente, puedes incluir también jQuery (requerido por Bootstrap JS) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+</body>
+
+</html>
